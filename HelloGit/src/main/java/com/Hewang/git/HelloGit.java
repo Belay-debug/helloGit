0fda4db6786211a13dc295e950ff70c2e6d39d20
belay-debug <zhanghaiwang23@126.com>
2020-06-04 10:10:05 +0800

    Master's change

--- HelloGit/src/main/java/com/Hewang/git/HelloGit.java
+++ HelloGit/src/main/java/com/Hewang/git/HelloGit.java
@@ -10,6 +10,8 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("Update version-1!");
         System.out.println("Branch-1!");
+        System.out.println("Master's change!");
+
     }
 
 }
